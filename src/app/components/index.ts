48a790a9f2a430987c21d28b7b61b51118d37172
shamuso <shamuso23@gmail.com>
2025-01-18 11:02:23 -0600

    Split out home page components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,10 @@
 import { HomeComponent } from "./home/home.component";
+import { TitleBarComponent } from "./title-bar/title-bar.component";
 
 export * from "./home/home.component"
+export * from "./title-bar/title-bar.component"
 
 export const COMPONENTS = [
-    HomeComponent
+    HomeComponent,
+    TitleBarComponent
 ]
---
+++ src/app/components/title-bar/title-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'title-bar',
+  templateUrl: './title-bar.component.html',
+  standalone: false,
+  styleUrl: './title-bar.component.scss'
+})
+export class TitleBarComponent {
+}
